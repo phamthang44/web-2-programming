7f00e32edd14066d2762818a8759b26542951684
phamthang44 <Ducthang4342@gmail.com>
2025-10-11 13:58:39 +0700

    week 3

---
+++ week-3/week-3/vue-for-scss/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { reactive } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+// 1. Dùng reactive() để tạo một đối tượng có tính phản ứng
+const form = reactive({
+  name: "",
+  // bạn có thể thêm các trường khác ở đây
+  // email: ''
+});
+
+// Giờ đây, Vue sẽ theo dõi tất cả các thuộc tính trong 'form'.
+</script>
+
+<template>
+  <h1 class="title">Hello Vue 3 + Vite + SCSS!</h1>
+  <div>
+    <input type="text" placeholder="Type something..." v-model="form.name" />
+  </div>
+
+  <p>Giá trị bạn đã nhập là: {{ form.name }}</p>
+
+  <HelloWorld msg="Vite + Vue" />
+</template>
