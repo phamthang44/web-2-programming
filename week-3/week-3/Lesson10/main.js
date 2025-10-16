587675f4d6310b2bc13b67496805bf2088ea933b
phamthang44 <Ducthang4342@gmail.com>
2025-10-16 11:29:59 +0700

    week 3

--- week-3/week-3/Lesson10/main.js
+++ week-3/week-3/Lesson10/main.js
@@ -1,24 +1,20 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart:0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
-        }
+  data() {
+    return {
+      author: "Pham Duc Thang - GCS230575",
+      cart: [],
+      premium: true,
+    };
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id);
     },
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateImage(variantImage) {
-            this.image = variantImage
-        }
-    }
-})
+    removeById(id) {
+      const index = this.cart.indexOf(id);
+      if (index > -1) {
+        this.cart.splice(index, 1);
+      }
+    },
+  },
+});
--- week-3/week-3/Lesson8/main.js
+++ week-3/week-3/Lesson8/main.js
@@ -1,24 +1,59 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart:0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
-        }
-    },
-    methods: {
-        addToCart() {
-            this.cart += 1
+  data() {
+    return {
+      author: "Pham Duc Thang - GCS230575",
+      cart: 0,
+      product: "Socks",
+      brand: "Vue Mastery",
+      image: "./assets/images/socks_blue.jpg",
+      inStock: false,
+      onSale: true,
+      selectedVariant: 0,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
         },
-        updateImage(variantImage) {
-            this.image = variantImage
-        }
-    }
-})
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
+      ],
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
+    updateVariant(index) {
+      this.selectedVariant = index;
+      this.image = this.variants[index].image;
+      this.inStock = this.variants[index].quantity;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    sale() {
+      if (this.onSale) {
+        return this.brand + " " + this.product + " is on sale.";
+      }
+      return "";
+    },
+  },
+});
